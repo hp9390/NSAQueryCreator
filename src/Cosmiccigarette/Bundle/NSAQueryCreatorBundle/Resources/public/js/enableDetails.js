6817064b06430a33aa901a5b421b02755c1b4471
Holger Piontek <hp9390@gmail.com>
2014-03-09 23:43:11 +0100

    Added brand to navbar again.

--- src/Cosmiccigarette/Bundle/NSAQueryCreatorBundle/Resources/public/js/enableDetails.js
+++ src/Cosmiccigarette/Bundle/NSAQueryCreatorBundle/Resources/public/js/enableDetails.js
@@ -1,16 +1,16 @@
 /**
  * Created by Holger on 09.03.14.
  */
-var detailsOne = function() {
+var detailsOne = function () {
     var searchTerm = $('#badgeSearchQueryOne').text();
     var html = '<div class="bg-info" id="infoSearchTerm"><p class="lead text-center text-info">Your search term is: ' + searchTerm + '</p></div>';
     $('#queryButtons').append(html);
 }
-var detailsTwo = function() {
+var detailsTwo = function () {
     var searchTerm = $('#badgeSearchQueryTwo').text();
     var html = '<div class="bg-info" id="infoSearchTerm"><p class="lead text-center text-info">Your search term is: ' + searchTerm + '</p></div>';
     $('#queryButtons').append(html);
 }
-var disableDetails = function() {
+var disableDetails = function () {
     $('#infoSearchTerm').remove();
 }
