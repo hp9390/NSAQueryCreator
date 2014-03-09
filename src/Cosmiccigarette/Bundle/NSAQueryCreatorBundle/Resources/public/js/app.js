e00829a51546515e9da874091c880ac023e9a108
Holger Piontek <hp9390@gmail.com>
2014-03-09 23:13:46 +0100

    Added on hover effect.

--- src/Cosmiccigarette/Bundle/NSAQueryCreatorBundle/Resources/public/js/app.js
+++ src/Cosmiccigarette/Bundle/NSAQueryCreatorBundle/Resources/public/js/app.js
@@ -2,7 +2,7 @@
  * Created by Holger on 08.03.14.
  */
 require(['jquery', 'knockoutjs'], function() {
-    require(['modernizr', 'bootstrap', 'jqueryManipulation', 'knockoutjsManipulation'], function() {
+    require(['modernizr', 'bootstrap', 'bootstrapTooltip', 'jqueryManipulation', 'knockoutjsManipulation'], function() {
         $( document ).ready(readyFunction);
     });
 });
